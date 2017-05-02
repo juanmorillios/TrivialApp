a73e1ad34900bebefffbbc805ebaf8fc462b6c8d
Juan Morillo <juanmorillios@gmail.com>
2017-05-02 16:44:36 +0200

    V1.0

---
+++ TrivialApp/CustomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomTableViewCell.swift
+//  TrivialApp
+//
+//  Created by Juan Morillo on 2/5/17.
+//  Copyright © 2017 Juan Morillo. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
